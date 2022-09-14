dfb1159e0fec91f85811e410fa5ba105c24f3ffb
dhanushsss <dhanush.sridhar01@gmail.com>
2022-09-14 19:48:42 +0530

    first commit

---
+++ src/components/upload-files.component.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import UploadService from "../services/upload-files.service";
+import "../components/upload.css";
+
+export default class UploadFiles extends Component {
+  constructor(props) {
+    super(props);
+    this.selectFile = this.selectFile.bind(this);
+    this.upload = this.upload.bind(this);
+
+    this.state = {
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: "",
+
+      fileInfos: [],
+    };
+  }
+
+  componentDidMount() {
+    UploadService.getFiles().then((response) => {
+      this.setState({
+        fileInfos: response.data,
+      });
+    });
+  }
+
+  selectFile(event) {
+    this.setState({
+      selectedFiles: event.target.files,
+    });
+  }
+
+  upload() {
+    let currentFile = this.state.selectedFiles[0];
+
+    this.setState({
+      progress: 0,
+      currentFile: currentFile,
+    });
+
+    UploadService.upload(currentFile, (event) => {
+      this.setState({
+        progress: Math.round((100 * event.loaded) / event.total),
+      });
+    })
+      .then((response) => {
+        this.setState({
+          message: response.data.message,
+        });
+        return UploadService.getFiles();
+      })
+      .then((files) => {
+        this.setState({
+          fileInfos: files.data,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          progress: 0,
+          message: "Could not upload the file!",
+          currentFile: undefined,
+        });
+      });
+
+    this.setState({
+      selectedFiles: undefined,
+    });
+  }
+
+  render() {
+    const { selectedFiles, message } = this.state;
+
+    return (
+      <div className="main">
+        <label className="btn btn-default">
+          <input type="file" onChange={this.selectFile} />
+        </label>
+
+        <button
+          className="btn btn-success"
+          disabled={!selectedFiles}
+          onClick={this.upload}
+        >
+          Upload
+        </button>
+
+        <div className="alert alert-light" role="alert">
+          {message}
+        </div>
+      </div>
+    );
+  }
+}
